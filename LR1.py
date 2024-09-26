43669dfe19b0c67907a20b2d08f492f9c8aed92f
Ivan Abramov <ivabramov16@gmail.com>
2024-09-26 15:07:31 +0300

    LR1 - Задание 6: Трансляция изображения с крестом на экране

--- LR1.py
+++ LR1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 #TASK 2
 def read_image():
@@ -76,4 +77,37 @@ def to_HSV_format():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-to_HSV_format()
+def read_camera():
+    cap = cv2.VideoCapture("http://192.168.0.103:8080/video")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        height, width, _ = frame.shape
+        cross_image = np.zeros((height, width, 3), dtype=np.uint8)
+        vertical_line_width = 60
+        vertical_line_height = 300
+        cv2.rectangle(
+            cross_image,
+            (width // 2 - vertical_line_width // 2, height // 2 - vertical_line_height // 2),
+            (width // 2 + vertical_line_width // 2, height // 2 + vertical_line_height // 2),
+            (0, 0, 255),
+            2
+        )
+        horizontal_line_width = 250
+        horizontal_line_height = 55
+        cv2.rectangle(
+            cross_image,
+            (width // 2 - horizontal_line_width // 2, height // 2 - horizontal_line_height // 2),
+            (width // 2 + horizontal_line_width // 2, height // 2 + horizontal_line_height // 2),
+            (0, 0, 255),
+            2
+        )
+        result_frame = cv2.addWeighted(frame, 1, cross_image, 0.5, 0)
+        cv2.imshow("Red Cross", result_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+read_camera()
