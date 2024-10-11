012949f127cdb0093a45225bed9e6fbfdb02b677
Ivan Abramov <ivabramov16@gmail.com>
2024-10-11 22:24:41 +0300

    LR2 - Задание 3: Морфологическое преобразование фильтрованного изображения

--- LR2.py
+++ LR2.py
@@ -44,4 +44,59 @@ def video_hsv_red():
     cap.release()
     cv2.destroyAllWindows()
 
-video_hsv_red()
+#TASK 3
+
+def erode(image, kernel):
+    m, n = image.shape
+    km, kn = kernel.shape
+    hkm = km // 2
+    hkn = kn // 2
+    eroded = np.copy(image)
+
+    for i in range(hkm, m - hkm):
+        for j in range(hkn, n - hkn):
+            eroded[i, j] = np.min(
+            image[i - hkm:i + hkm + 1, j - hkn:j + hkn + 1][kernel == 1]
+            )
+
+def dilate(image, kernel):
+    m, n = image.shape
+    km, kn = kernel.shape
+    hkm = km // 2
+    hkn = kn //2
+    dilated = np.copy(image)
+
+    for i in range(hkm, m - hkm):
+        for j in range(hkn, n - hkn):
+            dilated[i, j] = np.max(
+                image[i - hkm:i + hkm + 1, j - hkn:j + hkn + 1][kernel == 1]
+            )
+def morphological_transform():
+    cap = cv2.VideoCapture("http://192.168.0.104:8080/video")
+    if not cap.isOpened():
+        print("Невозможно открыть файл")
+        exit()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        low_red = np.array([0, 120, 220])
+        high_red = np.array([10, 255, 255])
+        mask = cv2.inRange(hsv, low_red, high_red)
+
+        kernel = np.ones((5, 5), np.uint8)
+
+        open_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        close_img = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+
+        cv2.imshow('open_frame', open_img)
+        cv2.imshow('close_frame', close_img)
+
+        if cv2.waitKey(30) & 0xFF == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+morphological_transform()
